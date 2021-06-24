435654ed77a5550614ae490f87f8da23bb47df4e
Kane Westwood <kane.westwood@googlemail.com>
2021-06-24 21:01:12 +0100

    first commit

---
+++ user-list/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * @returns {React.ReactElement} Main App
+ */
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
